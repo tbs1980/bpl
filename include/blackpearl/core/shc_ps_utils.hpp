6d62bd4734db8667f9e798ef8b91f626c325d9a4
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2016-06-13 23:23:23 +0100

    test for extract ps and create shc completed

--- include/blackpearl/core/shc_ps_utils.hpp
+++ include/blackpearl/core/shc_ps_utils.hpp
@@ -76,8 +76,8 @@ pow_spec<real_scalar_type> extract_pow_spec(
                     ps(mtpl_l,fld_i,fld_j) +=
                         2*(
                             shc_1(mtpl_l,mtpl_m,fld_i).real()*
-                            shc_2(mtpl_l,mtpl_m,fld_i).real()
-                            + shc_1(mtpl_l,mtpl_m,fld_j).imag()*
+                            shc_2(mtpl_l,mtpl_m,fld_j).real()
+                            + shc_1(mtpl_l,mtpl_m,fld_i).imag()*
                             shc_2(mtpl_l,mtpl_m,fld_j).imag()
                         );
                 }
@@ -93,9 +93,10 @@ pow_spec<real_scalar_type> extract_pow_spec(
     return ps;
 }
 
-template<typename real_scalar_type>
+template<typename real_scalar_type,class rng_type>
 sph_hrm_coeffs<real_scalar_type> create_gauss_sph_hrm_coeffs(
-    pow_spec<real_scalar_type> const & ps
+    pow_spec<real_scalar_type> const & ps,
+    rng_type & rng
 ){
     using namespace boost::numeric::ublas;
     typedef std::complex<real_scalar_type>  complex_scalar_type;
@@ -106,7 +107,8 @@ sph_hrm_coeffs<real_scalar_type> create_gauss_sph_hrm_coeffs(
     size_t const num_fields = ps.num_fields();
     sph_hrm_coeffs<real_scalar_type> shc(l_max,m_max,num_fields);
     const real_scalar_type sqrt_half = std::sqrt(0.5);
-    for(size_t mtpl_l=0;mtpl_l<l_max;++mtpl_l){
+    std::normal_distribution<real_scalar_type> norm_dist;
+    for(size_t mtpl_l = 0; mtpl_l <= l_max; ++mtpl_l){
         real_matrix_type ps_mat_l = ps(mtpl_l);
         real_matrix_type ps_chol_l
             = zero_matrix<real_scalar_type>(num_fields,num_fields);
@@ -116,23 +118,21 @@ sph_hrm_coeffs<real_scalar_type> create_gauss_sph_hrm_coeffs(
             "The power spectrum is not positive definite"
         );
         complex_vector_type shc_lm(num_fields);
-        std::mt19937 rng(1234);
-        std::normal_distribution<real_scalar_type> norm_dist;
-        for(size_t fld_i=0;fld_i<num_fields;++fld_i){
+        for(size_t fld_i = 0; fld_i < num_fields; ++fld_i){
             real_scalar_type re = norm_dist(rng);
             real_scalar_type im = 0;
             shc_lm(fld_i) = complex_scalar_type(re,im);
         }
         shc_lm = prod(ps_chol_l,shc_lm);
         shc.set_row(mtpl_l,0,shc_lm);
-        for(size_t mtpl_m=1;mtpl_m<=mtpl_l;++mtpl_m) {
-            for(size_t fld_i=0;fld_i<num_fields;++fld_i){
+        for(size_t mtpl_m = 1; mtpl_m <= mtpl_l; ++mtpl_m) {
+            for(size_t fld_i = 0; fld_i < num_fields; ++fld_i){
                 real_scalar_type re = norm_dist(rng);
                 real_scalar_type im = norm_dist(rng);
                 shc_lm(fld_i) = complex_scalar_type(re,im);
             }
             shc_lm = sqrt_half*prod(ps_chol_l,shc_lm);
-            shc.set_row(mtpl_l,0,shc_lm);
+            shc.set_row(mtpl_l,mtpl_m,shc_lm);
         }
     }
     return shc;
--- test/core/shc_ps_utils.cpp
+++ test/core/shc_ps_utils.cpp
@@ -6,6 +6,8 @@
 #include <chrono>
 #include <iostream>
 #include <complex>
+#include <limits>
+#include <random>
 #include <boost/test/unit_test.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
 #include <blackpearl/core/sph_hrm_coeffs.hpp>
@@ -16,14 +18,53 @@ void test_create_shc(){
     using namespace blackpearl::core;
     using namespace boost::numeric::ublas;
 
+    std::mt19937 rng(1234);
     std::size_t l_max = 32;
     std::size_t num_fields = 2;
     pow_spec<real_scalar_type> ps(l_max,num_fields);
+    pow_spec<real_scalar_type> ps_test_cum(l_max,num_fields);
     for(size_t mtpl_l=0;mtpl_l<=l_max;++mtpl_l){
-        ps(mtpl_l) = identity_matrix<double>(num_fields)*4.;
+        ps(mtpl_l) = identity_matrix<real_scalar_type>(num_fields);
+        ps_test_cum(mtpl_l) = zero_matrix<real_scalar_type>(num_fields);
     }
-
-    sph_hrm_coeffs<real_scalar_type> shc = create_gauss_sph_hrm_coeffs(ps);
+    size_t num_realzns = 1000;
+    for(size_t rlzn_i = 0; rlzn_i < num_realzns; ++rlzn_i ){
+        sph_hrm_coeffs<real_scalar_type> shc =
+            create_gauss_sph_hrm_coeffs(ps,rng);
+        pow_spec<real_scalar_type> ps_test =
+            extract_pow_spec<real_scalar_type>(shc,shc);
+        for(size_t mtpl_l = 0; mtpl_l <= l_max; ++mtpl_l){
+            ps_test_cum(mtpl_l) += ps_test(mtpl_l);
+        }
+    }
+    for(size_t mtpl_l = 0; mtpl_l <= l_max; ++mtpl_l) {
+        ps_test_cum(mtpl_l) /= real_scalar_type(num_realzns);
+    }
+    for(size_t mtpl_l = 0; mtpl_l <= l_max; ++mtpl_l) {
+        real_scalar_type err_mc(0.1);
+        for(size_t fld_i = 0; fld_i < num_fields; ++fld_i){
+            for(size_t fld_j = 0; fld_j < num_fields; ++fld_j){
+                if( fld_i == fld_j) {
+                    BOOST_REQUIRE(
+                        std::abs(
+                            ps_test_cum(mtpl_l,fld_i,fld_j)
+                            - real_scalar_type(1)
+                        )
+                        <= err_mc
+                    );
+                }
+                else {
+                    BOOST_REQUIRE(
+                        std::abs(
+                            ps_test_cum(mtpl_l,fld_i,fld_j)
+                            - real_scalar_type(0)
+                        )
+                        <= err_mc
+                    );
+                }
+            }
+        }
+    }
 }
 
 template<typename real_scalar_type>
@@ -34,33 +75,44 @@ void test_extract_pow_spec() {
 
     size_t const l_max = 32;
     size_t const m_max = l_max;
-    size_t const num_fields = 1;
+    size_t const num_fields = 2;
     pow_spec<real_scalar_type> ps(l_max,num_fields);
     for(size_t mtpl_l=0;mtpl_l<=l_max;++mtpl_l){
-        ps(mtpl_l) = identity_matrix<double>(num_fields)*4.;
+        ps(mtpl_l) = identity_matrix<real_scalar_type>(num_fields)*4.;
     }
 
     sph_hrm_coeffs<real_scalar_type> shc(l_max,m_max,num_fields);
     for(size_t mtpl_m = 0; mtpl_m <= m_max; ++mtpl_m){
         for(size_t mtpl_l = mtpl_m; mtpl_l <= l_max; ++mtpl_l) {
             shc(mtpl_l,mtpl_m,0) = complex_scalar_type(1.,0.);
+            shc(mtpl_l,mtpl_m,1) = complex_scalar_type(1.,0.);
         }
     }
 
     pow_spec<real_scalar_type> ps_test =
         extract_pow_spec<real_scalar_type>(shc,shc);
-
+    real_scalar_type eps = std::numeric_limits<real_scalar_type>::epsilon();
     for(size_t mtpl_l = 0; mtpl_l <= l_max; ++mtpl_l){
-        std::cout<<mtpl_l<<"\t"<<ps_test(mtpl_l,0,0)<<std::endl;
+        for(size_t fld_i = 0; fld_i < num_fields; ++fld_i){
+            for(size_t fld_j = 0; fld_j < num_fields; ++fld_j){
+                BOOST_REQUIRE(
+                    std::abs(
+                        ps_test(mtpl_l,fld_i,fld_j)
+                        - real_scalar_type(1)
+                    )
+                    <= eps
+                );
+            }
+        }
     }
 }
 
-// BOOST_AUTO_TEST_CASE(create_shc){
-//     test_create_shc<float>();
-//     test_create_shc<double>();
-// }
+BOOST_AUTO_TEST_CASE(create_shc){
+    test_create_shc<float>();
+    test_create_shc<double>();
+}
 
 BOOST_AUTO_TEST_CASE(extract_pow_spec){
-    //test_extract_pow_spec<float>();
+    test_extract_pow_spec<float>();
     test_extract_pow_spec<double>();
 }
